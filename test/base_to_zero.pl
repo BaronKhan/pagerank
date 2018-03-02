0f4b00d1259c4bed852c36c76ef78e7540a850c5
BaronKhan <bksonic@hotmail.co.uk>
2018-03-02 14:56:50 +0000

    Remove test files

--- test/base_to_zero.pl
+++
@@ -1,6 +0,0 @@
-use strict;
-
-while (<>) {
-  my @nodes = map {$_ - 1 } split;
-  print "@nodes" . "\n";
-}
--- test/produce_barabasi.R
+++
@@ -1,6 +0,0 @@
-library(igraph)
- 
-for (num.vertices in seq(2000000, 10000000, 1000000)) {
-  g <- barabasi.game(num.vertices)
-  write.graph(g,  sprintf("%s-%d.%s", "barabasi", num.vertices, "txt"))
-}
